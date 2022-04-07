ddbbdc2c0048df47bcbf17321275d19c511e71b0
Justin Zhang <judtin@seas.upenn.edu>
2022-04-07 01:04:45 -0400

    does this work

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,16 @@ const routes = require('./routes')
 
 const app = express();
 
+app.use(express.static('dist'))
+
 
 app.use(cors({ credentials: true, origin: ['http://localhost:1234'] }));
 
 app.get('/hello', routes.hello)
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'))
+  })
 
 app.listen(config.server_port, () => {
     console.log(`Server running at http://${config.server_host}:${config.server_port}/`);
