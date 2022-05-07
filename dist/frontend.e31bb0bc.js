dd7e04488e1da553c67a4ecb7cc096e93fc55122
Justin Zhang <judtin@seas.upenn.edu>
2022-05-07 15:12:00 -0400

    remove docs

--- dist/frontend.e31bb0bc.js
+++ dist/frontend.e31bb0bc.js
@@ -58215,7 +58215,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62618" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59585" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
